2a4a3b80bb6f78f5d84904a7fdd16b2948bac5c1
Enkk17 <86929986+Enkk17@users.noreply.github.com>
2022-04-06 22:03:19 +0200

    Add files via upload

---
+++ fuction for reading file.cpp
@@ -0,0 +1,22 @@
+string file_content() 
+{
+	// filestream variables 
+    fstream file;
+    string filename;
+ 
+    // we declare filename which corresponds to the file we want to open
+    filename = "password_test.txt";
+ 
+    //open the file
+    file.open(filename.c_str());
+  	
+	// create a stringstream
+	stringstream buffer;  
+	
+	//passes the file buffer to the stringstream
+	buffer << file.rdbuf();
+
+	//reading the file
+	return buffer.str(); 
+  	
+}
